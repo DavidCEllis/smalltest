84ca56985301f0756fbbaea31101f1956dd310d9
David C Ellis <ducksual@gmail.com>
2022-10-17 21:56:38 +0100

    Write tests for the individual functions

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -2,7 +2,7 @@ import unittest
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from smalltest.discover import discover_tests
+from smalltest.discover import discover_tests, discover_test_functions, discover_test_modules
 
 faketests = """
 
@@ -18,17 +18,17 @@ def notatest():
 
 
 class TestDicover(unittest.TestCase):
-    def test_single_file_full(self):
+    def test_discover_tests_single(self):
         with TemporaryDirectory() as tmpfolder:
             testfile = Path(tmpfolder) / "test_fake.py"
             testfile.write_text(faketests)
 
             expected = {testfile: ["test_fake", "test_real"]}
-            result = discover_tests(tmpfolder)
+            result = discover_tests(base_path=tmpfolder)
 
             self.assertEqual(expected, result)
 
-    def test_multiple_files(self):
+    def test_discover_tests_multiple(self):
         with TemporaryDirectory() as tmpfolder:
             testfile = Path(tmpfolder) / "test_fake.py"
             testfile.write_text(faketests)
@@ -40,6 +40,29 @@ class TestDicover(unittest.TestCase):
                 testfile: ["test_fake", "test_real"],
                 testfile2: ["test_fake", "test_real"]
             }
-            result = discover_tests(tmpfolder)
+            result = discover_tests(base_path=tmpfolder)
+
+            self.assertEqual(expected, result)
+
+    def test_discover_modules(self):
+        with TemporaryDirectory() as tmpfolder:
+            testfile = Path(tmpfolder) / "test_fake.py"
+            testfile.write_text(faketests)
+
+            testfile2 = Path(tmpfolder) / "fake_test.py"
+            testfile2.write_text(faketests)
+
+            expected = sorted([testfile, testfile2])
+            result = sorted(discover_test_modules(base_path=tmpfolder))
+
+            self.assertEqual(expected, result)
+
+    def test_discover_test_functions(self):
+        with TemporaryDirectory() as tmpfolder:
+            testfile = Path(tmpfolder) / "test_fake.py"
+            testfile.write_text(faketests)
+
+            expected = {testfile: ['test_fake', 'test_real']}
+            result = discover_test_functions(test_files=[testfile])
 
             self.assertEqual(expected, result)
