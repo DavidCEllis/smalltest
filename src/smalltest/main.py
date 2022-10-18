54fd3bdc7ad908a51b371b29a585bbcf2543cb6e
David C Ellis <ducksual@gmail.com>
2022-10-18 17:50:11 +0100

    Make test runner an entry point

--- src/smalltest/main.py
+++ src/smalltest/main.py
@@ -1,19 +1,23 @@
 """
 Perform the various combinations of discovering and running tests
 """
+import sys
+
 from smalltest.discover import discover_tests
 from smalltest.run import run_tests_serial
 from smalltest.reporter import text_reporter
 
 
-def discover_and_run(base_path):
+def discover_and_run():
+    try:
+        base_path = sys.argv[1]
+    except IndexError:
+        base_path = None
     tests = discover_tests(base_path)
     test_results = run_tests_serial(tests)
-    return test_results
+    print("-"*50)
+    text_reporter(test_results)
 
 
 if __name__ == "__main__":
-    from pathlib import Path
-    result = discover_and_run(Path.cwd())
-    print("-"*50)
-    text_reporter(result)
+    discover_and_run()
