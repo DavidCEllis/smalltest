5f25b7b72001b138d6f57aef9fb3d2cf17684c68
David C Ellis <ducksual@gmail.com>
2022-10-18 14:07:27 +0100

    Start thinking about the testrunner

--- src/smalltest/run.py
+++ src/smalltest/run.py
@@ -1,3 +1,40 @@
 """
 Run the tests
 """
+import warnings
+from collections import namedtuple
+from io import StringIO
+from contextlib import redirect_stderr, redirect_stdout
+
+TestResult = namedtuple("TestResult", ["Success", "ExceptionArgs", "stdout", "stderr", "warnings"])
+
+
+def run_test(test):
+    """
+    Run the test function
+    :param test
+    :return: TestResult
+    """
+    stdout = StringIO()
+    stderr = StringIO()
+    try:
+        with redirect_stdout(stdout), redirect_stderr(stderr), warnings.catch_warnings(record=True) as warns:
+            test()
+    except AssertionError as e:
+        result = TestResult(False, e.args, stdout.getvalue(), stderr.getvalue(), warns)
+    else:
+        result = TestResult(True, None, stdout.getvalue(), stderr.getvalue(), warns)
+
+    return result
+
+
+def load_test(module, testname):
+    """
+    Load the module into memory
+    Yield the test function
+    Clean up the module and assorted code
+
+    :param module: python file path for test file
+    :param testname: name of specific test to run
+    :return:
+    """
