253ee1f16da672c7ad7b79fe271be2a36afc30c4
David C Ellis <ducksual@gmail.com>
2022-10-18 15:51:26 +0100

    Create basic serial testrunner
    Create a copy of tests that use smalltest

--- src/smalltest/run.py
+++ src/smalltest/run.py
@@ -1,40 +1,66 @@
 """
-Run the tests
+Run the tests_unittest
 """
+import importlib.util
 import warnings
-from collections import namedtuple
-from io import StringIO
-from contextlib import redirect_stderr, redirect_stdout
 
-TestResult = namedtuple("TestResult", ["Success", "ExceptionArgs", "stdout", "stderr", "warnings"])
+from collections import namedtuple
+from contextlib import redirect_stderr, redirect_stdout
+from io import StringIO
+
+TestResult = namedtuple(
+    "TestResult",
+    ["success", "exception_args", "stdout", "stderr", "warnings"]
+)
 
 
 def run_test(test):
     """
-    Run the test function
-    :param test
+    Run the test function, capture stdout, stderr and uncaught warnings
+    to display in the report.
+
+    :param test function - callable
     :return: TestResult
     """
     stdout = StringIO()
     stderr = StringIO()
     try:
-        with redirect_stdout(stdout), redirect_stderr(stderr), warnings.catch_warnings(record=True) as warns:
+        with redirect_stdout(stdout), \
+             redirect_stderr(stderr), \
+             warnings.catch_warnings(record=True) as warns:
             test()
     except AssertionError as e:
-        result = TestResult(False, e.args, stdout.getvalue(), stderr.getvalue(), warns)
+        result = TestResult(
+            False, e.args, stdout.getvalue(), stderr.getvalue(), warns
+        )
     else:
-        result = TestResult(True, None, stdout.getvalue(), stderr.getvalue(), warns)
+        result = TestResult(
+            True, None, stdout.getvalue(), stderr.getvalue(), warns
+        )
 
     return result
 
 
-def load_test(module, testname):
-    """
-    Load the module into memory
-    Yield the test function
-    Clean up the module and assorted code
+def run_tests_serial(test_dict):
+    results = {}
+    for module_path, test_names in test_dict.items():
+        module_name = module_path.stem
+        spec = importlib.util.spec_from_file_location(module_name, module_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
 
-    :param module: python file path for test file
-    :param testname: name of specific test to run
-    :return:
-    """
+        results[module_name] = []
+
+        for test_name in test_names:
+            result = run_test(getattr(module, test_name))
+            if result.success:
+                print(f"{test_name} - Success")
+            else:
+                print(f"{test_name} - Failure")
+
+        results[module_name] = [
+            run_test(getattr(module, test_name))
+            for test_name in test_names
+        ]
+    return results
+
--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,4 +1,3 @@
-import unittest
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -8,61 +7,63 @@ faketests = """
 
 def test_fake():
     pass
-    
+
 def test_real():
     pass
-    
+
 def notatest():
     pass
 """
 
 
-class TestDicover(unittest.TestCase):
-    def test_discover_tests_single(self):
-        with TemporaryDirectory() as tmpfolder:
-            testfile = Path(tmpfolder) / "test_fake.py"
-            testfile.write_text(faketests)
+def test_discover_tests_single():
+    with TemporaryDirectory() as tmpfolder:
+        testfile = Path(tmpfolder) / "test_fake.py"
+        testfile.write_text(faketests)
 
-            expected = {testfile: ["test_fake", "test_real"]}
-            result = discover_tests(base_path=tmpfolder)
+        expected = {testfile: ["test_fake", "test_real"]}
+        result = discover_tests(base_path=tmpfolder)
 
-            self.assertEqual(expected, result)
+        assert expected == result
 
-    def test_discover_tests_multiple(self):
-        with TemporaryDirectory() as tmpfolder:
-            testfile = Path(tmpfolder) / "test_fake.py"
-            testfile.write_text(faketests)
 
-            testfile2 = Path(tmpfolder) / "fake_test.py"
-            testfile2.write_text(faketests)
+def test_discover_tests_multiple():
+    with TemporaryDirectory() as tmpfolder:
+        testfile = Path(tmpfolder) / "test_fake.py"
+        testfile.write_text(faketests)
 
-            expected = {
-                testfile: ["test_fake", "test_real"],
-                testfile2: ["test_fake", "test_real"]
-            }
-            result = discover_tests(base_path=tmpfolder)
+        testfile2 = Path(tmpfolder) / "fake_test.py"
+        testfile2.write_text(faketests)
 
-            self.assertEqual(expected, result)
+        expected = {
+            testfile: ["test_fake", "test_real"],
+            testfile2: ["test_fake", "test_real"]
+        }
+        result = discover_tests(base_path=tmpfolder)
 
-    def test_discover_modules(self):
-        with TemporaryDirectory() as tmpfolder:
-            testfile = Path(tmpfolder) / "test_fake.py"
-            testfile.write_text(faketests)
+        assert expected == result
 
-            testfile2 = Path(tmpfolder) / "fake_test.py"
-            testfile2.write_text(faketests)
 
-            expected = sorted([testfile, testfile2])
-            result = sorted(discover_test_modules(base_path=tmpfolder))
+def test_discover_modules():
+    with TemporaryDirectory() as tmpfolder:
+        testfile = Path(tmpfolder) / "test_fake.py"
+        testfile.write_text(faketests)
 
-            self.assertEqual(expected, result)
+        testfile2 = Path(tmpfolder) / "fake_test.py"
+        testfile2.write_text(faketests)
 
-    def test_discover_test_functions(self):
-        with TemporaryDirectory() as tmpfolder:
-            testfile = Path(tmpfolder) / "test_fake.py"
-            testfile.write_text(faketests)
+        expected = sorted([testfile, testfile2])
+        result = sorted(discover_test_modules(base_path=tmpfolder))
 
-            expected = {testfile: ['test_fake', 'test_real']}
-            result = discover_test_functions(test_files=[testfile])
+        assert expected == result
 
-            self.assertEqual(expected, result)
+
+def test_discover_test_functions():
+    with TemporaryDirectory() as tmpfolder:
+        testfile = Path(tmpfolder) / "test_fake.py"
+        testfile.write_text(faketests)
+
+        expected = {testfile: ['test_fake', 'test_real']}
+        result = discover_test_functions(test_files=[testfile])
+
+        assert expected == result
