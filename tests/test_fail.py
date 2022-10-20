f64a42d819e9a2962155ac5e7588a07e11f3dc31
David Ellis <ducksual@gmail.com>
2022-10-20 13:59:27 +0100

    Add some messages to the test xfailure

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -1,6 +1,9 @@
+import sys
 from smalltest.markers import xfail
 
 
 @xfail(True, "Test is always false.")
 def test_1_is_2():
+    sys.stdout.write("Captured STDOUT")
+    sys.stderr.write("Captured STDERR")
     assert 1 == 2, 'Does 1 == 2'
