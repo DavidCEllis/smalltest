ce465896ff56a9ea85dc2c137963a2e9818d791b
David Ellis <ducksual@gmail.com>
2022-10-20 18:07:32 +0100

    Rename markers to modifiers because they decorate and modify what happens inside the test code.

--- src/smalltest/tools/__init__.py
+++ src/smalltest/tools/__init__.py
@@ -1,2 +1,2 @@
-from ._markers import skip, skipif, xfail, XPassMarker, XFailMarker, SkipMarker
+from ._modifiers import skip, skipif, xfail, XPassMarker, XFailMarker, SkipMarker
 from ._exception import raises
